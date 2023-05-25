3748135b55406d4516debd2f671fbb4b34cad804
zawhtetaung211 <zawhtetaung21199@gmail.com>
2023-05-25 18:33:13 +0630

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1>Home Page</h1>
+        <p>This is home Page</p>
+      </div>
+    </main>
+  )
+}
